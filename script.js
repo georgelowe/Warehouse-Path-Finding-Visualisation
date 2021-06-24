535b4168684c86ed5c0bc302d477ce7cd3d0ddd4
George Lowe <georgeloweone@gmail.com>
2021-06-24 09:04:08 +0100

    Tile Object and basic grid population

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ var resultsMessage = document.getElementById("results-container");
 // Colours
 var startColour = "#0B9F00";
 var pickColour = "#f64900";
-var rackColour = "#f3f3f3";
+var rackColour = "#dbdbdb";
 
 // Tiles
 var canvas;
 var ctx;
 var numColumns = 45;
 var numRows = 25;
+var tileWidth = 20;
+var tileHeight = 20;
 
 // Event Listeners
 clearButton.addEventListener(
@@ -41,18 +43,52 @@ goButton.addEventListener(
   false
 );
 
+// Populate tileArray of tile objects
+var tileArray = [];
+for (let i = 0; i < numColumns; i++) {
+  tileArray[i] = [];
+  for (let j = 0; j < numRows; j++) {
+    tileArray[i][j] = new Tile(i * (20 + 1), j * (20 + 1));
+  }
+}
+
+tileArray[0][0].setStatus("start");
+
+// RE CONFIGURE HEIGHT AND WIDTHS
+tileArray[42][23].setStatus("start");
+
 function configure() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  drawSpace();
+  populateGrid();
 }
 
-function drawSpace() {
-  ctx.fillStyle = startColour;
+// Populate grid with tiles from tileArray
+function populateGrid() {
+  for (let i = 0; i < numColumns; i++) {
+    for (let j = 0; j < numRows; j++) {
+      drawTile(tileArray[i][j].x, tileArray[i][j].y, tileArray[i][j].colour);
+    }
+  }
+}
+
+function drawTile(x, y, state) {
+  console.log("drawing ");
+  ctx.fillStyle = state;
   ctx.beginPath();
-  ctx.rect(0, 0, 20, 20);
+  ctx.rect(x, y, 20, 20);
   ctx.closePath();
   ctx.fill();
 }
+
+// Calculate the path between two points
+// @param firstPoint, secondPoint, algorithm
+// @return cost
+function calculateCost() {}
+
+// Calculate the most efficient route
+// @param
+// @return
+function calculateBestRoute() {}
 
 configure();
---
+++ tile.js
@@ -0,0 +1,21 @@
+class Tile {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.status = "empty";
+    this.colour = "#dbdbdb";
+  }
+
+  setStatus(status) {
+    this.status = status;
+    if (status == "start") {
+      this.colour = "#0B9F00";
+    } else if (status == "empty") {
+      this.colour = "dbdbdb";
+    } else if (status == "racking") {
+      this.colour = "#000000";
+    } else if (status == "pick") {
+      this.colour == "#f64900";
+    }
+  }
+}
